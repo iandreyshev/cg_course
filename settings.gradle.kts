d97784f66f9f7109b4a8ef8aa607733841b62fc0
ivan.andreyshev <ivan.andreyshev@ispring.com>
2026-02-05 10:20:51 +0300

    Реорганизация структуры проекта
    - Все модули перемещены в директорию modules/
    - Обновлен settings.gradle.kts с путями к модулям
    - Улучшена читаемость структуры multi-module проекта

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,8 +21,19 @@ dependencyResolutionManagement {
 
 rootProject.name = "CGLabs"
 include(":app")
-include(":lab1")
 include(":core")
+include(":lab1")
 include(":lab2")
 include(":lab3")
 include(":lab4")
+include(":libs:ui")
+include(":libs:utils")
+
+// Указываем новое расположение модулей
+project(":core").projectDir = file("modules/core")
+project(":lab1").projectDir = file("modules/lab1")
+project(":lab2").projectDir = file("modules/lab2")
+project(":lab3").projectDir = file("modules/lab3")
+project(":lab4").projectDir = file("modules/lab4")
+project(":libs:ui").projectDir = file("modules/libs/ui")
+project(":libs:utils").projectDir = file("modules/libs/utils")
